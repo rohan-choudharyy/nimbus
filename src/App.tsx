b91f5205b9e977b48911dbe193ddc6bb3e4a0163
Rohan Choudhary <rohan.c2002@gmail.com>
2024-12-31 11:03:24 +0530

    dark mode and routing

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './components/layout'
+import { ThemeProvider } from './context/theme-provider'
+import WeatherDashboard from './pages/weather-dashboard'
+import CityPage from './pages/citypage'
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+      <ThemeProvider defaultTheme='dark'>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<WeatherDashboard/>}/>
+            <Route path='/city/:cityName' element={<CityPage/>}/>
+          </Routes>
+        </Layout>
+      </ThemeProvider>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/weather-dashboard.tsx
@@ -0,0 +1,9 @@
+
+
+const WeatherDashboard = () => {
+  return (
+    <div>WeatherDashboard</div>
+  )
+}
+
+export default WeatherDashboard
