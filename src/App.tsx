dcde25d143986f6364a9e74aba18b22c7d473ee0
Rohan Choudhary <rohan.c2002@gmail.com>
2024-12-31 22:07:38 +0530

    added tanstack-query and api

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Layout from './components/layout'
 import { ThemeProvider } from './context/theme-provider'
 import WeatherDashboard from './pages/weather-dashboard'
 import CityPage from './pages/citypage'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+const queryClient = new QueryClient();
 function App() {
-  
-
   return (
+    <QueryClientProvider client = {queryClient}>
     <BrowserRouter>
       <ThemeProvider defaultTheme='dark'>
         <Layout>
@@ -20,6 +22,8 @@ function App() {
         </Layout>
       </ThemeProvider>
     </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>
   )
 }
 
